92b2b119eebc7b5104b6c026e09cedfa512fcbef
kartikeyagup <kartikeyagupta@hotmail.com>
2016-09-05 14:37:18 +0530

    Added generator script

---
+++ experimental/statistics-analysis/scripts/generator.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def main():
+  for i in xrange(1,100):
+    print "Calling", i
+    dirname = "data/distance_" + str(i)
+    command = "build/sensitivity --distance="+str(i)+" --dirname="+dirname
+    # print command
+    # continue
+    subprocess.call(command, shell=True)
+
+if __name__ == '__main__':
+  main()
