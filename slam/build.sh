e4a6e8efcd3705a1e21dee49ce006cf1d2ac0ed1
kartikeyagup <kartikeyagupta@hotmail.com>
2016-10-03 23:24:13 +0530

    Added initial code for epipolar verification

--- slam/build.sh
+++ slam/build.sh
@@ -5,11 +5,24 @@ fi
 
 cd build
 cmake ..
-rm -rf slam
-make
-if [ -f slam ]; then 
-  cd ..
-  rm -rf $2
-  mkdir $2
-  build/slam -dirname=$2 -video=$1
+if [ $1 = "slam" ]; then 
+  rm -rf slam 
+else
+  rm -rf epipolar
+fi
+make -j4
+if [ $1 = "slam" ]; then
+  if [ -f slam ]; then 
+    cd ..
+    if [ -d $3 ]; then
+      rm -rf $3
+      mkdir $3
+      build/slam -dirname=$3 -video=$2
+    fi
+  fi
+else 
+  if [ -f  epipolar ]; then
+    cd ..
+    build/epipolar
+  fi
 fi
