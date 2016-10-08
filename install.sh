e8b8328b15a4cbe89719fe30dbc1a4efef7a1f92
kartikeyagup <kartikeyagupta@hotmail.com>
2016-10-09 01:17:47 +0530

    Fixed install script and gitignore

--- install.sh
+++ install.sh
@@ -1,46 +1,46 @@
 # KEEP UBUNTU OR DEBIAN UP TO DATE
 
-sudo apt-get -E -y update
-sudo apt-get -E -y upgrade
-sudo apt-get -E -y dist-upgrade
-sudo apt-get -E -y autoremove
+sudo -E apt-get -y update
+sudo -E apt-get -y upgrade
+sudo -E apt-get -y dist-upgrade
+sudo -E apt-get -y autoremove
 
 
 # INSTALL THE DEPENDENCIES
 
 # Build tools:
-sudo apt-get -E install -y build-essential cmake
+sudo -E apt-get install -y build-essential cmake
 
 # GFlags
-sudo apt-get -E install -y libgflags-dev
+sudo -E apt-get install -y libgflags-dev
 
 # GUI (if you want to use GTK instead of Qt, replace 'qt5-default' with 'libgtkglext1-dev' and remove '-DWITH_QT=ON' option in CMake):
-sudo apt-get -E install -y qt5-default libvtk6-dev
+sudo -E apt-get install -y qt5-default libvtk6-dev
 
 # Media I/O:
-sudo apt-get -E install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+sudo -E apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
 
 # Video I/O:
-sudo apt-get -E install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+sudo -E apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
 
 # Parallelism and linear algebra libraries:
-sudo apt-get -E install -y libtbb-dev libeigen3-dev
+sudo -E apt-get install -y libtbb-dev libeigen3-dev
 
 # Python:
-sudo apt-get -E install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
+sudo -E apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
 
 # Java:
-sudo apt-get -E install -y ant default-jdk
+sudo -E apt-get install -y ant default-jdk
 
 # Documentation:
-sudo apt-get -E install -y doxygen
+sudo -E apt-get install -y doxygen
 
 # OpenGL
-sudo apt-get -E install -y freeglut3 freeglut3-dev
+sudo -E apt-get install -y freeglut3 freeglut3-dev
 
 # INSTALL THE LIBRARY (YOU CAN CHANGE '3.1.0' FOR THE LAST STABLE VERSION)
 
-sudo apt-get -E install -y unzip wget
+sudo -E apt-get install -y unzip wget
 wget https://github.com/Itseez/opencv/archive/3.1.0.zip
 unzip 3.1.0.zip
 mv opencv-3.1.0 OpenCV
