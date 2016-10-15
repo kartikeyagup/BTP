060f01ef6f94291a92d26d0253f84519ccf47e32
kartikeyagup <kartikeyagupta@hotmail.com>
2016-10-15 14:50:10 +0530

    Removed comments

--- slam/scripts/merge_pt_cloud.cpp
+++ slam/scripts/merge_pt_cloud.cpp
@@ -18,28 +18,8 @@ int main(int argc, char **argv)
   nvm_file f2(FLAGS_path2 + "outputVSFM_GB.nvm");
   std::cerr << "Done reading 2\n";
 
-  // f2.NormaliseScale(get_best_scaling_factor(f1, f2));
-  // f2.NormaliseScale(get_best_scaling_factor(f1, f2));
-
-  // f2.NormaliseInternalTranslation(0);
-
-  // f2.NormaliseTranslation(-f1.kf_data.rbegin()->translation);
-  // f2.NormaliseTranslation(get_best_translation(f1, f2));
-  // get_best_translation(f1, f2);
-  // get_best_translation(f1, f2);
-  // f2.NormaliseInternalRotation(0);
-  // f1.NormaliseInternalRotation(f1.kf_data.size()-1);
-  // f2.NormaliseRotation(f1.kf_data.rbegin()->rotation.transpose());
-
   GetBestRST(f1, f2);
 
-  // for (auto it: f2.kf_data) {
-  //   f1.kf_data.push_back(it);
-  // }  
-
-  // for (auto it: f2.corr_data) {
-  //   f1.corr_data.push_back(it);
-  // }
   nvm_file merged = merge_nvm(f1, f2);
 
   merged.save_to_disk(FLAGS_output_dir + "combined.nvm");
